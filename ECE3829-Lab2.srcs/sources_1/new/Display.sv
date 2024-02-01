a6225e59369507ebbbe78a55b058498c338c9604
Liam Snow <liamsnow03@gmail.com>
2024-02-01 14:58:41 -0500

    Finished Display (untested)

--- ECE3829-Lab2.srcs/sources_1/new/Display.sv
+++ ECE3829-Lab2.srcs/sources_1/new/Display.sv
@@ -3,16 +3,21 @@
 module Display(
     input wire CLK25, reset_n,
     input wire [1:0] state,
-    input wire stateOverride,
+    input wire blockEnabled,
     output reg [11:0] vgaColor,
-    input wire vgaX, vgaY
+    input wire [9:0] vgaX, vgaY
 );
 
+    //VGA Params
+    localparam [9:0] VGA_WIDTH = 640;
+    localparam [9:0] VGA_HEIGHT = 480;
+    localparam [9:0] VGA_MIDDLE = VGA_WIDTH >> 1;
+
     //State
-    localparam YELLOW_SCREEN = 2'b00; //completely yellow screen
-    localparam RED_WHITE_BARS = 2'b01; //vertical alternating red white bars (16 wide)
-    localparam GREEN_BLOCK = 2'b10; //black screen w/ green block (128x128) in top right
-    localparam BLUE_STRIPE = 2'b11; //black screen w/ hori blue stripe (32 wide) at bottom
+    localparam [1:0] YELLOW_SCREEN = 2'b00; //completely yellow screen
+    localparam [1:0] RED_WHITE_BARS = 2'b01; //vertical alternating red white bars (16 wide)
+    localparam [1:0] GREEN_BLOCK = 2'b10; //black screen w/ green block (128x128) in top right
+    localparam [1:0] BLUE_STRIPE = 2'b11; //black screen w/ hori blue stripe (32 high) at bottom
 
     //Colors
     localparam [11:0] WHITE  = {4'hF, 4'hF, 4'hF};
@@ -22,28 +27,65 @@ module Display(
     localparam [11:0] BLUE   = {4'h0, 4'h0, 4'hF};
     localparam [11:0] YELLOW = {4'hF, 4'hF, 4'h0};
 
-    always_ff @(posedge CLK25 or reset_n) begin
+    //Block
+    localparam [5:0] BLOCK_SIZE = 32;
+    localparam [4:0] BLOCK_HALF_SIZE = BLOCK_SIZE >> 1;
+    localparam [9:0] BLOCK_LEFT_X = VGA_MIDDLE - BLOCK_HALF_SIZE - 1;
+    localparam [9:0] BLOCK_RIGHT_X = VGA_MIDDLE + BLOCK_HALF_SIZE + 1;
+    localparam [9:0] BLOCK_BOTTOM_Y = VGA_HEIGHT - BLOCK_SIZE - 1;
+    reg [9:0] blockY = 0;
+    reg blockMovingDown = 0;
+
+    localparam [22:0] BLOCK_SLOW_CLOCK_END = 6_250_000-1; //25MHz -> 2Hz
+    reg [22:0] blockClockCounter = 0;
+    reg blockClock = 0; //2Hz
+
+    //Sync Logic
+    always_ff @(posedge CLK25 or negedge reset_n) begin
         if (~reset_n) begin
-        
+             vgaColor <= BLACK;
+             blockY <= 0;
+             blockMovingDown <= 0;
+             blockClockCounter <= 0;
+             blockClock <= 0;
         end
-        else if (stateOverride) begin
-            //blue screen w/ red block (32x32) that moves down at 2hz
+
+        //Block
+        else if (blockEnabled) begin
+            //Draw Block
+            if (vgaX > BLOCK_LEFT_X &&
+                vgaX < BLOCK_RIGHT_X &&
+                vgaY >= blockY &&
+                vgaY <= blockY + BLOCK_SIZE) begin
+                vgaColor <= RED;
+            end
+            else vgaColor <= BLUE;
+
+            //Block Clock
+            if (blockClockCounter == BLOCK_SLOW_CLOCK_END) begin
+                blockClock <= ~blockClock;
+                blockClockCounter <= 0;
+            end else blockClockCounter <= blockClockCounter + 1;
+
+            //Block Movement (2Hz)
+            if (blockClock) begin
+                if (blockMovingDown ?
+                    (blockY < BLOCK_BOTTOM_Y) :
+                    (blockY > 1)) begin
+                    blockY <= blockMovingDown ? (blockY + 1) : (blockY - 1);
+                end
+                else begin
+                    blockMovingDown <= ~blockMovingDown;
+                end
+            end
         end
+
+        //Normal States
         else case (state)
-            YELLOW_SCREEN: begin
-                vgaColor <= YELLOW;
-            end
-            RED_WHITE_BARS: begin
-                //vertical alternating red white bars (16 wide)
-            end
-            GREEN_BLOCK: begin
-                //black screen w/ green block (128x128) in top right
-            end
-            BLUE_STRIPE: begin
-                //black screen w/ hori blue stripe (32 wide) at bottom
-            end
+            YELLOW_SCREEN: vgaColor <= YELLOW;
+            RED_WHITE_BARS: vgaColor <= (~vgaX[4]) ? RED : WHITE;
+            GREEN_BLOCK: vgaColor <= (vgaY < 128 && vgaX > (VGA_WIDTH - 128)) ? GREEN : BLACK; 
+            BLUE_STRIPE: vgaColor <= (vgaY > (VGA_HEIGHT - 32)) ? BLUE : BLACK;
         endcase
     end
-
 endmodule
-
